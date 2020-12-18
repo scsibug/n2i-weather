67e6cf4da1c0a30f7c7d9e9592e5ceeff8ca3caf
Greg Heartsfield <scsibug@imap.cc>
2020-12-18 17:48:45 -0600

    Extract temperature

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 // Receive NATS weather messages, and forward to InfluxDB.
+use influxdb::InfluxDbWriteable;
+use chrono::{DateTime, Utc};
 
-fn main() {
+#[derive(InfluxDbWriteable)]
+    struct WeatherReading {
+        time: DateTime<Utc>,
+        temp: f64,
+        feels_like: f64,
+        humidity: f64,
+        #[tag] zipcode: String,
+    }
+
+
+#[async_std::main]
+async fn main() {
     println!("Connecting to NATS");
     let ncres = nats::connect("nats.wellorder.net");
     let nc = match ncres {
@@ -19,6 +32,9 @@ fn main() {
             std::process::exit(1);
         }
     };
+    // Connect to influxdb
+    println!("Connecting to InfluxDB");
+    let client = influxdb::Client::new("http://ektar.wellorder.net:8086", "iot");
     for msg in sub.messages() {
         println!("Received Message!");
         println!("This message subject is: {}", msg.subject);
@@ -27,8 +43,35 @@ fn main() {
             Ok(s) => s,
             Err(e) => { std::process::exit(1) }
         };
-        println!("Message is: {}", msgstr)
-//        let event: CloudEvent = cloudevents::from_str(data).unwrap();
+        println!("Message is: {}", msgstr);
+        // Build a JSON deserializer for the message
+        let event : cloudevents::event::Event = serde_json::from_str(msgstr).unwrap();
+        println!("{}", event);
+        let payload = match event.data().unwrap() {
+            cloudevents::Data::Json(v) => v,
+            _ => { 
+                println!("Did not match JSON payload");
+                std::process::exit(1);
+            }
+        }; 
+        println!("{}", payload);
+        // extract fields from payload
+        let mainobj = match payload {
+            serde_json::value::Value::Object(m) => m,
+            _ => {
+                println!("Expected a top-level object");
+                std::process::exit(1);
+            }
+        };
+        // extract temp from mainobj
+        println!("{}", mainobj.get("temp").unwrap())
+        // parse the data payload
+        //
+        //let vr: Result<serde_json::Value, serde_json::error::Error> = serde_json::from_str(event.data().unwrap());
+//        event.deserialize(msgstr)
+//        let parsed_event = serde::from_str(msgstr).unwrap();
+
+        // Need to run iter_attributes over the parsed Event
     }
 
 }
