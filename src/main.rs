200186347ded0104465ff90ce9b16de1f016a087
Greg Heartsfield <scsibug@imap.cc>
2020-12-18 20:39:03 -0600

    Add additional fields from weather event

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Receive NATS weather messages, and forward to InfluxDB.
 use influxdb::InfluxDbWriteable;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 
 #[derive(InfluxDbWriteable)]
     struct WeatherReading {
@@ -8,9 +8,15 @@ use chrono::{DateTime, Utc};
         temp: f64,
         feels_like: f64,
         humidity: f64,
+        pressure: f64,
+        uvi: f64,
+        visibility: f64,
+        wind_deg: f64,
+        wind_speed: f64,
+        dew_point: f64,
+        clouds: f64,
         #[tag] zipcode: String,
     }
-
 
 #[async_std::main]
 async fn main() {
@@ -64,9 +70,45 @@ async fn main() {
             }
         };
         // extract temp from mainobj
-        println!("{}", mainobj.get("temp").unwrap())
+        println!("{}", mainobj.get("temp").unwrap());
+        let temp = mainobj.get("temp").unwrap().as_f64().unwrap();
+        // feels like temp
+        let feels_like = mainobj.get("feels_like").unwrap().as_f64().unwrap();
+        // humiditiy
+        let humidity = mainobj.get("humidity").unwrap().as_f64().unwrap();
+        // pressure
+        let pressure = mainobj.get("pressure").unwrap().as_f64().unwrap();
+        // wind
+        let wind_deg = mainobj.get("wind_deg").unwrap().as_f64().unwrap();
+        let wind_speed = mainobj.get("wind_speed").unwrap().as_f64().unwrap();
+        // dew point
+        let dew_point = mainobj.get("dew_point").unwrap().as_f64().unwrap();
+        // visibility
+        let visibility = mainobj.get("visibility").unwrap().as_f64().unwrap();
+        // uvi
+        let uvi = mainobj.get("uvi").unwrap().as_f64().unwrap();
+        // clouds
+        let clouds = mainobj.get("uvi").unwrap().as_f64().unwrap();
         // parse the data payload
-        //
+        let dt = Utc.timestamp(mainobj.get("dt").unwrap().as_i64().unwrap(), 0); 
+        println!("{}", dt);
+        let wr = WeatherReading {
+            time: dt,
+            temp: temp,
+            feels_like: feels_like,
+            humidity: humidity,
+            pressure: pressure,
+            wind_deg: wind_deg,
+            wind_speed: wind_speed,
+            dew_point: dew_point,
+            uvi: uvi,
+            visibility: visibility,
+            clouds: clouds,
+            zipcode: "76021".to_string()
+        }; 
+        let write_result = client
+            .query(&wr.into_query("weather")).await;
+        assert!(write_result.is_ok(), "Write result to influxdb was not okay");
         //let vr: Result<serde_json::Value, serde_json::error::Error> = serde_json::from_str(event.data().unwrap());
 //        event.deserialize(msgstr)
 //        let parsed_event = serde::from_str(msgstr).unwrap();
